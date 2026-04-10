8899248899ec0e32cf436c9857e8fd24d72bcca3
Ibtissam EL <ibtissamel742@gmail.com>
2026-04-10 13:22:01 +0100

    feat: add the utilisateurs Sidebar item && added its route in routes.jsx

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,7 +1,7 @@
-// routes.jsx
 import ProtectedRoute from './ProtectedRoute';
 import { GestionPage, HomePage, SettingsPage } from '../pages';
 import AuthPage from '../pages/Auth/AuthPage';
+import UsersPage from '../pages/Users/UsersPage';  // create this page
 
 export const publicRoutes = [
   { path: '/login', element: AuthPage },
@@ -9,15 +9,21 @@ export const publicRoutes = [
 
 export const protectedRoutes = [
   {
-    element: <ProtectedRoute allowedRoles={['user', 'admin', 'superuser']} />,
-    children: [
-      { path: '/', element: HomePage },
+    element  : <ProtectedRoute allowedRoles={['user', 'admin', 'superuser']} />,
+    children : [
+      { path: '/',           element: HomePage },
       { path: '/parametres', element: SettingsPage },
     ],
   },
   {
-    element: <ProtectedRoute allowedRoles={['admin', 'superuser']} />,
-    children: [
+    element  : <ProtectedRoute allowedRoles={['admin', 'superuser']} />,
+    children : [
+      { path: '/utilisateurs', element: UsersPage },
+    ],
+  },
+  {
+    element  : <ProtectedRoute allowedRoles={['superuser']} />,
+    children : [
       { path: '/gestion', element: GestionPage },
     ],
   },
