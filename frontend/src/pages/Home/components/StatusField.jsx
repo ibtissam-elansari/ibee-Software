7a843992b50c85ad0e67311e14a035ce3ea1eb7c
Ibtissam EL <ibtissamel742@gmail.com>
2026-04-10 16:28:36 +0100

    feat: modified the cardsContent to have dynamic data

--- frontend/src/pages/Home/components/StatusField.jsx
+++ frontend/src/pages/Home/components/StatusField.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import StatusCard from './StatusCard';
-import cardsContent from '../config/cardsContent';
+import cards from '../config/cardsContent'
 
 
 const StatusField = () => {
   return (
     <div className="flex flex-col gap-y-4 p-4 bg-base-100 rounded-2xl overflow-hidden w-full border border-base-200">
-      {/* <h3 className="text-2xl font-bold text-primary">Les Ruches</h3> */}
-      <div className="">
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          {cardsContent.map((card) => (
-            <StatusCard card={card}/>
-          ))}
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {cards.map((card, i) => (
+          <StatusCard key={i} card={card} />
+        ))}
       </div>
     </div>
   )
