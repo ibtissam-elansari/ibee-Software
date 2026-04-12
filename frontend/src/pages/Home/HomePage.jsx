2d10c7849f326a0a0c68bd274aac15747256691d
Ibtissam EL <ibtissamel742@gmail.com>
2026-04-12 20:16:27 +0100

    added basic hives listing and toolbar

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -5,7 +5,10 @@ const HomePage = () => {
   return (
     <div className="flex flex-col gap-4 p-4 h-full">
       <StatusField />
-
+      <HivesField
+        onHiveClick={(hive) => navigate(`/hives/${hive.id}`)}
+        onAddHive={() => setAddModalOpen(true)}
+      />
     </div>
   )
 }
