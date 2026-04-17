de724f4afc406eaf2bc532d5d0da017816e97ff7
Ibtissam EL <ibtissamel742@gmail.com>
2026-04-17 12:04:24 +0100

    fix: changed r.date to r.data in the updateUser endpoint

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -4,6 +4,6 @@ import http from './client';
 export const login      = (data)     => http.post('/auth/login', data).then(r => r.data);
 export const getMe      = ()         => http.get('/auth/me').then(r => r.data);
 export const getUsers   = ()         => http.get('/auth/users').then(r => r.data);
-export const createUser = (data)     => http.post('/auth/users', data).then(r => r.date);
+export const createUser = (data)     => http.post('/auth/users', data).then(r => r.data);
 export const updateUser = (id, data) => http.patch(`/auth/users/${id}`, data).then(r => r.data);
 export const deleteUser = (id)       => http.delete(`/auth/users/${id}`);
