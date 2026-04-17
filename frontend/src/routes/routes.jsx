09289f4a3b45312b57740a46338ef39da4c9b43c
Ibtissam ELANSARI <ibtissamel742@gmail.com>
2026-04-17 12:22:45 +0100

    Deleted Users folder

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,7 +1,6 @@
 import ProtectedRoute from './ProtectedRoute';
 import { GestionPage, HomePage, SettingsPage } from '../pages';
 import AuthPage from '../pages/Auth/AuthPage';
-import UsersPage from '../pages/Users/UsersPage';
 import HiveAnalyticsPage from '../pages/Analytics/HiveAnalyticsPage';
 import MetricDetailPage from '../pages/Analytics/MetricDetailPage';
 
@@ -15,20 +14,21 @@ export const protectedRoutes = [
     children : [
       { path: '/',           element: HomePage },
       { path: '/parametres', element: SettingsPage },
+      { path: '/gestion',   element: GestionPage},
+      { path: '/gestion/:hiveId',                      element: HiveAnalyticsPage   },
+      { path: '/gestion/:hiveId/details/:metric',      element: MetricDetailPage    },
     ],
   },
-  {
-    element  : <ProtectedRoute allowedRoles={['admin', 'superuser']} />,
-    children : [
-      { path: '/utilisateurs', element: UsersPage },
-    ],
-  },
+  // {
+  //   element  : <ProtectedRoute allowedRoles={['admin', 'superuser']} />,
+  //   children : [
+  //     { path: '/utilisateurs', element: UsersPage },
+  //   ],
+  // },
   {
     element  : <ProtectedRoute allowedRoles={['superuser']} />,
     children : [
-      { path: '/gestion',                              element: GestionPage         },
-      { path: '/gestion/:hiveId',                      element: HiveAnalyticsPage   },
-      { path: '/gestion/:hiveId/details/:metric',      element: MetricDetailPage    },
+
     ],
   },
 ];
