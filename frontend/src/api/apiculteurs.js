6b8e6faf2728b4f5b7d2fb1816a6f0a59bfd6a83
Ibtissam ELANSARI <ibtissamel742@gmail.com>
2026-04-21 11:34:08 +0100

    updated apiculteurs.js and useApiculteurs hook

--- frontend/src/api/apiculteurs.js
+++ frontend/src/api/apiculteurs.js
@@ -1,6 +1,11 @@
 import http from './client';
 
-export const getApiculteurs  = ()           => http.get('/api/apiculteurs').then(r => r.data);
-export const createApiculteur= (data)       => http.post('/api/apiculteurs', data).then(r => r.data);
-export const updateApiculteur= (id, data)   => http.patch(`/api/apiculteurs/${id}`, data).then(r => r.data);
-export const deleteApiculteur= (id)         => http.delete(`/api/apiculteurs/${id}`);
+export const getApiculteurs   = ()           => http.get('/api/apiculteurs').then(r => r.data);
+export const getApiculteur    = (id)         => http.get(`/api/apiculteurs/${id}`).then(r => r.data);
+export const createApiculteur = (data)       => http.post('/api/apiculteurs', data).then(r => r.data);
+export const updateApiculteur = (id, data)   => http.patch(`/api/apiculteurs/${id}`, data).then(r => r.data);
+export const deleteApiculteur = (id)         => http.delete(`/api/apiculteurs/${id}`);
+
+// Scoped to one apiculteur's data
+export const getApiculteurHives         = (id) => http.get(`/api/apiculteurs/${id}/hives`).then(r => r.data);
+export const getApiculteurNotifications = (id) => http.get(`/api/apiculteurs/${id}/notifications`).then(r => r.data);
